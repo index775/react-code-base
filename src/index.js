ce8f3e682bf8e4e7fd5f8bc5479ce4fc2c6adb6a
Manish <mdashsharma95@gmail.com>
2024-01-13 17:22:34 +0530

    React setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Your React components are gearing up! 🚀
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App />);
